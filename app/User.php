4849036324df176c4dbc78d0a89295e4e504c57d
Esteban Novo <novo.esteban@gmail.com>
2019-08-21 10:04:21 -0300

    Usando Eloquent ORM de forma interactiva con Tinker

--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function isAdmin(){
+        return $this->email === 'dulio@styde.net';
+    }
+
+    public static function findByEmail($email)
+    {
+        return static::where(compact('email'))->first();
+
+    }
 }
