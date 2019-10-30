29cdf80ca260e3c2094b0a7bd157ac28b51203e4
Esteban Novo <novo.esteban@gmail.com>
2019-10-30 12:39:02 -0300

    Crea un Panel de Control con Laravel: 9 . Instalación y configuración de Laravel Dusk

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Styde.net
+                    Laravel
                 </div>
 
                 <div class="links">
